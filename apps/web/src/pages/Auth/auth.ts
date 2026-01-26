b79335f61fe4e7240eeea2dce0986d4877c130e0
nitzan8897 <nitzanavargil1@gmail.com>
2026-01-26 13:03:09 +0200

    Change to relative import

--- apps/web/src/pages/Auth/auth.ts
+++ apps/web/src/pages/Auth/auth.ts
@@ -1,4 +1,4 @@
-import api from '../../lib/api';
+import api from '@/lib/api';
 import { IAuthResponse, IUserLogin, IUserRegistration } from '@rootshare/shared-types';
 
 export interface AuthError {
